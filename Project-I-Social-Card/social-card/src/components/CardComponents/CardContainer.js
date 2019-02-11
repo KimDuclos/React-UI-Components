49d95237fb5c12e814deb8e997f75832885f4622
Kim Duclos <kimduclos@gmail.com>
2019-02-11 14:24:27 -0700

    rearranged components and containers to the right files

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Card.css';
+
+
+const CardContainer = () => {
+    return (
+        // MVP linked <CardContainer /> to ReactJS site
+        <a href="http://www.reactjs.org">" <div className="card">
+            <CardBanner />
+            <CardContent />
+        </div></a>
+    )
+}
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderTitle = () => {
+    return (
+        <div className="headTitle">
+            <h4>Lambda School</h4><p>@LambdaSchool &middot 26 Jan</p>
+        </div>
+    );
+}
